25b3d84d3112aba81e2ece6c0a5b5869df9aa44d
shadybonne <shadybonne@gmail.com>
2022-06-07 11:36:20 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print main
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+_putchar('m');
+_putchar('a');
+_putchar('i');
+_putchar('n');
+_putchar('\n');
+
+return (0);
+}
